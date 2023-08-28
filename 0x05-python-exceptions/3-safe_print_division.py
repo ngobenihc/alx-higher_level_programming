cde902ba80cbc729b2daa602ed3294821a4d700a
ngobenihc <ngobenihc@yahoo.com>
2023-08-28 12:57:23 +0200

    magic

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """
-    divides two integers and prints the result
-    catches divide by zero exception
-    """
+    
+    #divides two integers and prints the result catches divide by zero exception
+    
     try:
-        res = a / b
+        divi = a / b
     except:
-        res = None
+        divi = None
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(divi))
+    return divi
