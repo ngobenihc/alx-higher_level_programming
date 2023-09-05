1f0e100f1013fb73c24a2cf8b380f78b73f0b93e
ngobenihc <ngobenihc@yahoo.com>
2023-09-05 13:33:37 +0200

    0x07

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,5 +4,5 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """a rectangle."""
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
