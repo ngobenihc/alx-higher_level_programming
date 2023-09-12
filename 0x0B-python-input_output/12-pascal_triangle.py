303ebda2fa9874b22a0bd83e1a126f4688f6a8c4
ngobenihc <ngobenihc@yahoo.com>
2023-09-12 17:09:41 +0200

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-
+"""Module containing the function pascal_triangle"""
 
 
 def pascal_triangle(n):
-    
+    """Returns a list of lists of integers representing the Pascal’s,
+    triangle of n.
+
+    Args:
+        n (int): rows of triangle.
+
+    Returns:
+        list: lists of lists of integers.
+    """
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+    pascal = [[1]]
+    for i in range(n - 1):
+        pascal.append([x + n for x, n in zip(pascal[-1] + [0],
+                                             [0] + pascal[-1])])
+    return (pascal)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+"""Module that loads, adds and saves arguments to a Python list"""
+from sys import argv
 
 
-import sys
+load_file = __import__('6-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+try:
+    json_list = load_file('add_item.json')
+except (ValueError, FileNotFoundError):
+    json_list = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+for item in argv[1:]:
+    json_list.append(item)
+
+save_file(json_list, 'add_item.json')
