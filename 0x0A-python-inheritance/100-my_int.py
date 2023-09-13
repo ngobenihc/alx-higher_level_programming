c6a26b994eaba95d586ac732173ee9f34497a62d
ngobenihc <ngobenihc@yahoo.com>
2023-09-13 16:41:21 +0200

    0x0A-python-inheritance test

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,12 +4,12 @@
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    
 
     def __eq__(self, value):
-        """Override == opeartor with != behavior."""
+        
         return self.real != value
 
     def __ne__(self, value):
-        """Override != operator with == behavior."""
+        
         return self.real == value
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Defines a Rectangle subclass Square."""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -8,11 +8,7 @@ class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+        
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
