dd6d6ea4d68e11217713a0bb8d5175baea04d490
ngobenihc <ngobenihc@yahoo.com>
2023-12-01 15:15:44 +0200

    1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
-
+"""script for testing status of web pages
+"""
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    import urllib.request
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
+        print_str = '''Body response:
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
+        print(print_str)
